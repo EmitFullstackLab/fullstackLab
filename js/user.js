a0bb6b9dd459b8306c2395f3095152928a1224a4
MattiaTagliani <125219020+MattiaTagliani@users.noreply.github.com>
2023-10-17 09:04:48 +0200

    cleaned up

--- js/user.js
+++ js/user.js
@@ -1,23 +1,31 @@
 const form = document.querySelector("form");
 const emailEl = document.querySelector("#email");
 const passwordEl = document.querySelector("#password");
-const confirmEl = document.querySelector("#confirmPwd");
+// const confirmEl = document.querySelector("#confirmPwd");
+
+async function fetchAsync(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const email = emailEl.value;
   const password = passwordEl.value;
-  const confirmPwd = confirmEl.value;
+  // const confirmPwd = confirmEl.value;
 
-  if (password === confirmPwd) {
-    console.log("pwd: ", passwordEl.value);
-    console.log("confirm: ", confirmEl.value);
-    console.log("password uguali");
-  } else {
-    console.log("pwd: ", passwordEl.value);
-    console.log("confirm: ", confirmEl.value);
-    console.log("password sbagliata");
-  }
+  // if (password === confirmPwd) {
+  //   console.log("pwd: ", passwordEl.value);
+  //   console.log("confirm: ", confirmEl.value);
+  //   console.log("password uguali");
+  // } else {
+  //   console.log("confirm: ", confirmEl.value);
+  //   console.log("password sbagliata");
+  // }
 
-  console.log("email: ", emailEl.value);
+  console.log("email: ", email);
+  console.log("pwd: ", password);
+
+  window.location.replace("/pages/register.html");
 });
