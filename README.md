32184607db8a25a239b158728adb13b9396f6b23
MattiaTagliani <125219020+MattiaTagliani@users.noreply.github.com>
2023-10-18 14:01:27 +0200

    setup of .env file (instructions on readme) - created simple index file with no css

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fullstackLab
+
+make .env file in main folder with this setup
+MYSQL_HOST=localhost
+MYSQL_DATABASE=name_of_database
+MYSQL_PORT=port_for_mysql
+MYSQL_USER=your_user_name
+MYSQL_PASSWORD=your_password
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*------ENVIRONMENT CONFIG------*/
+require("dotenv").config();
+
+const HOST = process.env.MYSQL_HOST;
+const DATABASE = process.env.MYSQL_DATABASE;
+const PORT = process.env.MYSQL_PORT;
+const USER = process.env.MYSQL_USER;
+const PASSWORD = process.env.MYSQL_PASSWORD;
+
+/*------EXPRESS CONFIG------*/
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
@@ -7,4 +17,28 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
 app.listen(3000);
+
+/*------MYSQL CONFIG------*/
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  port: PORT,
+  database: DATABASE,
+});
+
+let qStudents = "select * from students";
+connection.query(qStudents, (err, res) => {
+  // console.log(res);
+});
